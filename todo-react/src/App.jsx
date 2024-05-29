f6ea2b1a083bc89b1e8295a284a39df28d335e1a
HAN ZAW HTET <hanzawhtet317@gmail.com>
2024-05-29 15:41:32 +0630

    npm i MUI

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Item from "./Item";
 
 const api = "http://localhost:8181/todoList";
@@ -6,6 +6,9 @@ const api = "http://localhost:8181/todoList";
 export default function App(){
   const [data, setData] = useState([]);
   const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const inputRef = useRef();
 
   useEffect(() => {
 	(async () => {
@@ -27,7 +30,17 @@ export default function App(){
   return (
 	<div>
 		<h1>TODO-LIST</h1>
-
+		<hr/>
+		<form onSubmit={e => {
+			e.preventDefault();
+			const name = inputRef.current.value;
+			if(!name){
+				return false;
+			}
+		}}>
+			<input type="text" ref={inputRef} />
+			<input type="submit" />
+		</form>
 		<ul>
 			{data.filter(item => !item.done).map(item => {
 				return (
